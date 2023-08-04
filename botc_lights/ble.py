a978eca79e5a40e5abb03d149fe3cca488df2ea7
Alexander Wilde <alexander.wilde87@gmail.com>
2023-08-04 01:45:57 +0100

    Remove ble write_no_response

--- botc_lights/ble.py
+++ botc_lights/ble.py
@@ -7,49 +7,42 @@ STATE_CHARACTERISTIC = aioble.Characteristic(
   SERVICE,
   bluetooth.UUID('AAD80981-48AC-4763-8848-513873A69E15'),
   write=True,
-  write_no_response=True,
   capture=True,
 )
 PLAYER_LIVING_CHARACTERISTIC = aioble.Characteristic(
   SERVICE,
   bluetooth.UUID('AAD80982-48AC-4763-8848-513873A69E15'),
   write=True,
-  write_no_response=True,
   capture=True,
 )
 PLAYER_TYPE_CHARACTERISTIC = aioble.Characteristic(
   SERVICE,
   bluetooth.UUID('AAD80983-48AC-4763-8848-513873A69E15'),
   write=True,
-  write_no_response=True,
   capture=True,
 )
 PLAYER_TEAM_CHARACTERISTIC = aioble.Characteristic(
   SERVICE,
   bluetooth.UUID('AAD80984-48AC-4763-8848-513873A69E15'),
   write=True,
-  write_no_response=True,
   capture=True,
 )
 PLAYER_NOMINATED_CHARACTERISTIC = aioble.Characteristic(
   SERVICE,
   bluetooth.UUID('AAD80985-48AC-4763-8848-513873A69E15'),
   write=True,
-  write_no_response=True,
   capture=True,
 )
 BRIGHTNESS_CHARACTERISTIC = aioble.Characteristic(
   SERVICE,
   bluetooth.UUID('AAD80986-48AC-4763-8848-513873A69E15'),
   write=True,
-  write_no_response=True,
   capture=True,
 )
 COLORS_CHARACTERISTIC = aioble.Characteristic(
   SERVICE,
   bluetooth.UUID('AAD80987-48AC-4763-8848-513873A69E15'),
   write=True,
-  write_no_response=True,
   capture=True,
 )
 aioble.register_services(SERVICE)
